96fed6807b513bf5598ec6e45a4af4f47eacee59
e-rojas <ejrojas@icloud.com>
2020-08-28 21:35:52 -0300

    database connection to server successful

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 require("dotenv").config();
-port = 4444;
+port = process.env.PORT;
+
+require("./database/db"); //required db
 
 app.listen(port, () => {
-  console.log("Server listening on port " + port);
+  console.log("Server listening on port" + port);
 });
---
+++ server/database/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    err ? console.log(err) : console.log("Connected to MongoDB");
+  }
+);
